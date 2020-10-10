ff8762f62ede80602d4dd8c11cacabcd9ff52f4f
Francisco Dias <franciscopdneto@gmail.com>
2020-10-09 22:05:20 -0300

    DFS implemented. Change on some decision problems on code.

--- src/search.py
+++ src/search.py
@@ -19,7 +19,9 @@ def getPossibleStartCoords(width, height, map):
     for lineIdx, line in enumerate(map):
         for valueIdx, value in enumerate(line):
             if (lineIdx in [0, width - 1] or valueIdx in [0, height - 1]) and value != constant.OBSTACLE:
-                possibleStartCoords.append(State((lineIdx, valueIdx), 0))
+                coord = (lineIdx, valueIdx)
+                possibleStartCoords.append(State(coord, 0, \
+                    Output(1, 1 if value == constant.LOCALIZATION_POINT else 0, coord)))
     
     return possibleStartCoords
 
@@ -33,49 +35,53 @@ y, x, w = [int(i) for i in next(fileEntry).split()]
 map = [[i for i in line.rstrip('\n')] for line in fileEntry]
 
 class State:
-    def __init__(self, coord : tuple, cost = 0):
+    def __init__(self, coord : tuple, cost = 0, output : Output = Output()):
         self.coord = coord
         self.cost = cost
-        self.output = Output()
+        self.currentW = 0
+        self.output = output
     
     def __eq__(self, other):
         return self.coord[0] == other.coord[0] and self.coord[1] == other.coord[1]
 
 class Graph:
-    def __init__(self, root : State, parent = None, children : list = []):
+    def __init__(self, root : State, parent = None):
         self.root = root
-        self.children = children
         self.parent = parent
-        self.currentW = 0
+        self.children = []
 
-    def addChild(self, move : tuple, cost, parent, explored : list) -> None:
+    def addChild(self, move : tuple, cost : float, explored : list) -> None:
         if (move[0] > 0 and move[0] < y and \
             move[1] > 0 and move[1] < x and \
-            (move[0], move[1]) not in explored and \
+            (move not in explored) and \
             map[move[0]][move[1]] != constant.OBSTACLE):
 
-            if self.currentW == w and map[move[0]][move[1]] != constant.LOCALIZATION_POINT:
+            if self.root.currentW == w and map[move[0]][move[1]] != constant.LOCALIZATION_POINT:
                 return None
 
-            self.children.append(Graph(State(move, cost), parent))
-            if (map[move[1]][move[0]] == constant.LOCALIZATION_POINT):
-                self.currentW = 0
+            numTimesPassedLocationPoint = self.root.output.numTimesPassedLocationPoint + \
+                (1 if map[move[0]][move[1]] == constant.LOCALIZATION_POINT else 0)
+            currentW = self.root.currentW + \
+                (1 if map[move[0]][move[1]] == constant.LOCALIZATION_POINT else -self.root.currentW)
+            output = Output(self.root.output.numSteps + 1, numTimesPassedLocationPoint, self.root.output.coordsInitPoint)
+            self.children.append(Graph(State(move, cost, output), self))
+    
+    def __str__(self):
+        return "State {} from parent {}".format(self.root.coord, self.parent)
 
     def expandChildren(self, explored : list, heuristic : bool = False) -> None:
         # Respectivamente BAIXO, CIMA, ESQUERDA, DIREITA.
-        possibleMoves = [(self.root.coord[0], self.root.coord[1] + 1),\
-            (self.root.coord[0], self.root.coord[1] - 1),\
+        possibleMoves = [(self.root.coord[0] + 1, self.root.coord[1]),\
             (self.root.coord[0] - 1, self.root.coord[1]),\
-            (self.root.coord[0] + 1, self.root.coord[1])]
+            (self.root.coord[0], self.root.coord[1] - 1),\
+            (self.root.coord[0], self.root.coord[1] + 1)]
         
         if (not heuristic):
             for move in possibleMoves:
-                self.addChild(move, self.root.cost + 1, self, explored)
+                self.addChild(move, self.root.cost + 1, explored)
         else:
             for move in possibleMoves:
                 self.addChild(move, self.root.cost + 1 + heuristic(move), self, explored)
-
-        self.currentW += 1
 
 
 def bfsSearch():
@@ -94,9 +100,7 @@ def dfsSearch(currentGraph : Graph, maxDepth : int, explored : list) -> Output:
 
     currentGraph.expandChildren(explored)
     for child in currentGraph.children:
-        print("Child ({}, {}) cost {}".format(child.root.coord[0], child.root.coord[1], child.root.cost))
         possibleGoal = dfsSearch(child, maxDepth - 1, explored)
-        print("Possible goal {}".format(possibleGoal))
         if possibleGoal != None:
             return possibleGoal
 
