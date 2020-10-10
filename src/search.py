2af710d984d1b316bc8e443a35533f06367a498a
Francisco Dias <franciscopdneto@gmail.com>
2020-10-10 16:57:18 -0300

    Adapting to check all possible initial state.

--- src/search.py
+++ src/search.py
@@ -14,16 +14,16 @@ class Output:
         return "{} {} [{}, {}]".format(self.numSteps, self.numTimesPassedLocationPoint, \
             self.coordsInitPoint[0], self.coordsInitPoint[1])
 
-def getPossibleStartCoords(width, height, map):
-    possibleStartCoords = []
+def getInitialGraph(width, height, map):
+    possibleStartGraphs = []
     for lineIdx, line in enumerate(map):
         for valueIdx, value in enumerate(line):
             if (lineIdx in [0, width - 1] or valueIdx in [0, height - 1]) and value != constant.OBSTACLE:
                 coord = (lineIdx, valueIdx)
-                possibleStartCoords.append(State(coord, 0, \
-                    Output(1, 1 if value == constant.LOCALIZATION_POINT else 0, coord)))
+                possibleStartGraphs.append(Graph(State(coord, 1, 0, \
+                    Output(1, 1 if value == constant.LOCALIZATION_POINT else 0, coord)), []))
     
-    return possibleStartCoords
+    return Graph(State((-2, -2)), possibleStartGraphs)
 
                     
 
@@ -35,20 +35,20 @@ y, x, w = [int(i) for i in next(fileEntry).split()]
 map = [[i for i in line.rstrip('\n')] for line in fileEntry]
 
 class State:
-    def __init__(self, coord : tuple, cost = 0, output : Output = Output()):
+    def __init__(self, coord : tuple, cost = 0, currentW = 0, output : Output = Output()):
         self.coord = coord
         self.cost = cost
-        self.currentW = 0
+        self.currentW = currentW
         self.output = output
     
     def __eq__(self, other):
         return self.coord[0] == other.coord[0] and self.coord[1] == other.coord[1]
 
 class Graph:
-    def __init__(self, root : State, parent = None):
+    def __init__(self, root : State, children : list, parent = None):
         self.root = root
         self.parent = parent
-        self.children = []
+        self.children = children
 
     def addChild(self, move : tuple, cost : float, explored : list) -> None:
         if (move[0] > 0 and move[0] < y and \
@@ -62,12 +62,14 @@ class Graph:
             numTimesPassedLocationPoint = self.root.output.numTimesPassedLocationPoint + \
                 (1 if map[move[0]][move[1]] == constant.LOCALIZATION_POINT else 0)
             currentW = self.root.currentW + \
-                (1 if map[move[0]][move[1]] == constant.LOCALIZATION_POINT else -self.root.currentW)
+                (1 if map[move[0]][move[1]] != constant.LOCALIZATION_POINT else -1*self.root.currentW)
+            
             output = Output(self.root.output.numSteps + 1, numTimesPassedLocationPoint, self.root.output.coordsInitPoint)
-            self.children.append(Graph(State(move, cost, output), self))
+            child = Graph(State(move, cost, currentW, output), [], self)
+            self.children.append(child)
     
     def __str__(self):
-        return "State {} from parent {}".format(self.root.coord, self.parent)
+        return "State {} from parent {} from children {}".format(self.root.coord, self.parent, self.children)
 
     def expandChildren(self, explored : list, heuristic : bool = False) -> None:
         # Respectivamente BAIXO, CIMA, ESQUERDA, DIREITA.
@@ -90,13 +92,13 @@ def bfsSearch():
 
 def dfsSearch(currentGraph : Graph, maxDepth : int, explored : list) -> Output:
     currentState = currentGraph.root
-    if map[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
+    if currentState.coord[0] >= 0 and map[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
         return currentState.output
 
     if maxDepth == 0:
         return None
 
-    explored.append((currentState.coord[0], currentState.coord[1]))
+    explored.append(currentState.coord)
 
     currentGraph.expandChildren(explored)
     for child in currentGraph.children:
@@ -122,16 +124,12 @@ bestRoute = None
 if (algorithm.lower() == "bfs"):
     bestRoute = bfsSearch()
 elif (algorithm.lower() == "dfs"):
-    for initialState in getPossibleStartCoords(y, x, map):
-        bestRoute = dfsSearch(Graph(initialState), float("Inf"), [])
-        if bestRoute:
-            break
+    initialGraph = getInitialGraph(y, x, map)
+    bestRoute = dfsSearch(initialGraph, float("Inf"), [])
 elif (algorithm.lower() == "ids"):
     # TODO Pensar na profundidade máxima do algoritmo
-    for initialState in getPossibleStartCoords(y, x, map):
-        bestRoute = idsSearch(Graph(initialState), 10)
-        if bestRoute:
-            break
+    initialGraph = getInitialGraph(y, x, map)
+    bestRoute = idsSearch(initialGraph, y * x)
 elif (algorithm.lower() == "a*"):
     bestRoute = aStarSearch()
 else:
