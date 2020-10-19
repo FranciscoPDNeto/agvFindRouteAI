f334536b9acb27ebff8b261f89b65a42ddd0ec87
Felipe Ribeiro <felipelrib@gmail.com>
2020-10-19 02:29:50 -0300

    Rename 'map' variable, as it was redeclaring the 'map' function

--- src/search.py
+++ src/search.py
@@ -20,7 +20,7 @@ entry = sys.argv[2]
 
 fileEntry = open(entry, 'r')
 y, x, w = [int(i) for i in next(fileEntry).split()]
-map = [[i for i in line.rstrip('\n')] for line in fileEntry]
+industryMap = [[i for i in line.rstrip('\n')] for line in fileEntry]
 
 class State:
     def __init__(self, coord : tuple, cost = 0, currentW = 0, output : Output = Output()):
@@ -42,15 +42,15 @@ class Graph:
         if (move[0] > 0 and move[0] < y and \
             move[1] > 0 and move[1] < x and \
             (move not in explored) and \
-            map[move[0]][move[1]] != constant.OBSTACLE):
+            industryMap[move[0]][move[1]] != constant.OBSTACLE):
 
-            if self.root.currentW == w and map[move[0]][move[1]] != constant.LOCALIZATION_POINT:
+            if self.root.currentW == w and industryMap[move[0]][move[1]] != constant.LOCALIZATION_POINT:
                 return None
 
             numTimesPassedLocationPoint = self.root.output.numTimesPassedLocationPoint + \
-                (1 if map[move[0]][move[1]] == constant.LOCALIZATION_POINT else 0)
+                (1 if industryMap[move[0]][move[1]] == constant.LOCALIZATION_POINT else 0)
             currentW = self.root.currentW + \
-                (1 if map[move[0]][move[1]] != constant.LOCALIZATION_POINT else -1*self.root.currentW)
+                (1 if industryMap[move[0]][move[1]] != constant.LOCALIZATION_POINT else -1*self.root.currentW)
 
             output = Output(self.root.output.numSteps + 1, numTimesPassedLocationPoint, self.root.output.coordsInitPoint)
             child = Graph(State(move, cost, currentW, output), [], self)
@@ -73,16 +73,16 @@ class Graph:
             for move in possibleMoves:
                 self.addChild(move, self.root.cost + 1 + heuristic(move), explored)
 
-def getGoalCoords(map) -> tuple:
-    for lineIdx, line in enumerate(map):
+def getGoalCoords(industryMap) -> tuple:
+    for lineIdx, line in enumerate(industryMap):
         for valueIdx, value in enumerate(line):
             if value == constant.COLLECT_POINT:
                 return (lineIdx, valueIdx)
     raise Exception("There's no collect point in the map.")
 
-def getInitialGraph(width, height, map) -> Graph:
+def getInitialGraph(width, height, industryMap) -> Graph:
     possibleStartGraphs = []
-    for lineIdx, line in enumerate(map):
+    for lineIdx, line in enumerate(industryMap):
         for valueIdx, value in enumerate(line):
             if (lineIdx in [0, width - 1] or valueIdx in [0, height - 1]) and value != constant.OBSTACLE:
                 coord = (lineIdx, valueIdx)
@@ -99,7 +99,7 @@ def bfsSearch(graph : Graph) -> Output:
     while nodesQueue:
         node = nodesQueue.pop(0)
         currentState = node.root
-        if currentState.coord[0] >= 0 and map[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
+        if currentState.coord[0] >= 0 and industryMap[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
             return currentState.output
         visitedNodesCoords.append(currentState.coord)
         node.expandChildren(visitedNodesCoords)
@@ -107,7 +107,7 @@ def bfsSearch(graph : Graph) -> Output:
 
 def dlsSearch(currentGraph : Graph, maxDepth : int, explored : list) -> Output:
     currentState = currentGraph.root
-    if currentState.coord[0] >= 0 and map[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
+    if currentState.coord[0] >= 0 and industryMap[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
         return currentState.output
 
     if maxDepth == 0:
@@ -135,7 +135,7 @@ def idsSearch(initialGraph : Graph, maxDepth : int) -> Output:
     return None
 
 def aStarSearch(graph):
-    goalCoords = getGoalCoords(map)
+    goalCoords = getGoalCoords(industryMap)
     # Define a heurística do A*, que será a Distância de Manhattan.
     manhattanDistanceHeuristic = lambda move : abs(move[0] - goalCoords[0]) + abs(move[1] - goalCoords[1])
 
@@ -147,7 +147,7 @@ def aStarSearch(graph):
         node = min(nodesList, key=lambda node: node.root.cost)
         nodesList.remove(node)
         currentState = node.root
-        if currentState.coord[0] >= 0 and map[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
+        if currentState.coord[0] >= 0 and industryMap[currentState.coord[0]][currentState.coord[1]] == constant.COLLECT_POINT:
             return currentState.output
         visitedNodesCoords.append(currentState.coord)
         node.expandChildren(visitedNodesCoords, manhattanDistanceHeuristic)
@@ -155,16 +155,16 @@ def aStarSearch(graph):
 
 bestRoute = None
 if (algorithm.lower() == "bfs"):
-    initialGraph = getInitialGraph(y, x, map)
+    initialGraph = getInitialGraph(y, x, industryMap)
     bestRoute = bfsSearch(initialGraph)
 elif (algorithm.lower() == "dfs"):
-    initialGraph = getInitialGraph(y, x, map)
+    initialGraph = getInitialGraph(y, x, industryMap)
     bestRoute = dfsSearch(initialGraph, [])
 elif (algorithm.lower() == "ids"):
-    initialGraph = getInitialGraph(y, x, map)
+    initialGraph = getInitialGraph(y, x, industryMap)
     bestRoute = idsSearch(initialGraph, y * x)
 elif (algorithm.lower() == "a_star"):
-    initialGraph = getInitialGraph(y, x, map)
+    initialGraph = getInitialGraph(y, x, industryMap)
     bestRoute = aStarSearch(initialGraph)
 else:
     print("Wrong algorithm passed, please insert a valid one.")
